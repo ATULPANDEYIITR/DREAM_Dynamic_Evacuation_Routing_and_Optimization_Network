7bbc1e8c06c5fd7ba2d1b22e19a036406041a069
Atul Pandey <atulpandey.iitr@gmail.com>
2026-09-15 12:50:28 +0530

    Initialize project structure

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "DREAM_Dynamic_Evacuation_Routing_and_Optimization_Network" << std::endl;
+    std::cout << "Project initialized successfully." << std::endl;
+
+    return 0;
+}
